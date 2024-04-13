f7eb93699f03bd4d679673552a0d073d09102db6
zeonzy <zandvliet_leon@hotmail.com>
2024-04-14 00:42:16 +0200

    added ch2.6

--- src/main.py
+++ src/main.py
@@ -1,30 +1,31 @@
-from textnode import TextNode
-
-def text_node_to_html_node(text_node):
-    if text_node == "ext_type_text":
-        pass
-    elif text_node == "text_type_bold":
-        pass
-    elif text_node == "text_type_italic":
-        pass
-    elif text_node == "text_type_code":
-        pass
-    elif text_node == "text_type_link":
-        pass
-    elif text_node == "text_type_image":
-        pass
-    else:
-        raise Exception("Unknown TextNode type")
+from textnode import TextNode, text_node_to_html_node
 
 def main():
-    dummy_node1 = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    dummy_node2 = TextNode("note2", "test", "localhost")
-    dummy_node3 = TextNode("note2", "test", "localhost")
+    dummy_node1 = TextNode("This is a text node", "text")
+    dummy_node2 = TextNode("Dit een bolde claim", "bold")
+    dummy_node3 = TextNode("deze is schuin", "italic")
+    dummy_node4 = TextNode("wat code", "code")
+    dummy_node5 = TextNode("een linkje", "link", "https://www.boot.dev")
+    dummy_node6 = TextNode("This is a text node", "image", "https://www.boot.dev")
 
+    print(text_node_to_html_node(dummy_node1))
+    print(text_node_to_html_node(dummy_node2))
+    print(text_node_to_html_node(dummy_node3))
+    print(text_node_to_html_node(dummy_node4))
+    print(text_node_to_html_node(dummy_node5))
+    print(text_node_to_html_node(dummy_node6))
 
+'''
     print(dummy_node1)
     print(dummy_node2)
     print(dummy_node1 == dummy_node2)
     print(dummy_node2 == dummy_node3)
+
+    print(dummy_node2.text)
+    print(dummy_node2.text_type)
+    print(dummy_node2.url)
+'''
+
+
 
 main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from htmlnode import LeafNode
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text # The text content of the node
@@ -16,3 +18,19 @@ class TextNode:
         # returns a string representation of the TextNode object. It should look like this:
         # TextNode(TEXT, TEXT_TYPE, URL)
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+    if text_node.text_type == "text_type_text" or text_node.text_type == "text":
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == "text_type_bold" or text_node.text_type == "bold":
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == "text_type_italic" or text_node.text_type == "italic":
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == "text_type_code" or text_node.text_type == "code":
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type == "text_type_link" or text_node.text_type == "link":
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == "text_type_image" or text_node.text_type == "image":
+        return LeafNode("img",None, {"src": text_node.url, "alt": text_node.text})
+    else:
+        raise Exception("Unknown TextNode type")
